08129883f30ab52fc788a855d0d74da60efbf7eb
Stephanie <s.a.james5@gmail.com>
2018-02-07 21:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function johnLennonFacts(array)
     }
     
 function iLoveTheBeatles(number)
-
-do {var array = []} {array.push('I love the Beatles!')}
+{var array = []}
+  do {array.push('I love the Beatles!')}
     while (number < 7) 
     number++
     return ['I love the Beatles!']
